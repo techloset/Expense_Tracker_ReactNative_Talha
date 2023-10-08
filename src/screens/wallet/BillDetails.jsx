40da0f95e16826603bd889279d58c2ea26308050
talhaunas1 <mtalha4600@gmail.com>
2023-10-08 20:28:47 +0500

    responsive wallet screens

--- src/screens/wallet/BillDetails.jsx
+++ src/screens/wallet/BillDetails.jsx
@@ -1,24 +1,29 @@
-import {StyleSheet, Text, View, Image,TouchableOpacity} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  useWindowDimensions,
+  Dimensions,
+} from 'react-native';
 import React from 'react';
-import WalletComponentScreen from '../../component/transcationComponent/WalletComponentScreen';
+
 import youtubeCardBills from '../../assets/Images/youtubeCardBills.png';
 import CreditCard from '../../assets/Images/creditcard.svg';
 import Dot from '../../assets/Images/dot.svg';
 import PaypalIcon from '../../assets/Images/paypalogo.svg';
 import EmptyDot from '../../assets/Images/emptyDot.svg';
-// import Butoon from '../../component/button/Butoon';
-
 import Rectangular from '../../assets/Images/Rectangle.svg';
 import Ellipse from '../../assets/Images/Ellipse.svg';
 import Notification from '../../assets/Images/notification.svg';
 import LessThan from '../../assets/Images/lessThan.png';
 
 const BillDetails = ({navigation}) => {
+  const styles = useStyles();
   return (
-    <View style={{width: 414, height: 896}}>
-
-
-<View>
+    <View>
+      <View>
         <View style={styles.container_inner}>
           <Rectangular />
           <Ellipse style={styles.eelispse} />
@@ -27,12 +32,14 @@ const BillDetails = ({navigation}) => {
           <View style={styles.lessthen}>
             <Image style={{width: 23, height: 23}} source={LessThan} />
           </View>
+          <View>
+            <Text style={styles.billText}>Bill Details</Text>
+          </View>
           <View style={styles.notification}>
             <Notification />
           </View>
         </View>
       </View>
-
 
       {/* <WalletComponentScreen /> */}
 
@@ -98,9 +105,9 @@ const BillDetails = ({navigation}) => {
         </View>
 
         <TouchableOpacity onPress={() => navigation.navigate('BillPayment')}>
-        <View style={styles.bttn}>
-          <Text style={styles.bttnText}>Pay Now</Text>
-        </View>
+          <View style={styles.bttn}>
+            <Text style={styles.bttnText}>Pay Now</Text>
+          </View>
         </TouchableOpacity>
       </View>
     </View>
@@ -109,202 +116,196 @@ const BillDetails = ({navigation}) => {
 
 export default BillDetails;
 
-const styles = StyleSheet.create({
-  container: {
-    // flex:1,
-    position: 'absolute',
-    backgroundColor: 'white',
-    width: '100%',
-    // height:'290%',
-    marginTop: 165,
-    borderTopLeftRadius: 30,
-    borderTopRightRadius: 30,
-  },
+function useStyles() {
+  const {width, height} = useWindowDimensions();
+  // console.log()
+  return StyleSheet.create({
+    container: {
+      position: 'absolute',
+      backgroundColor: 'white',
+      width: '100%',
 
-  youtube_image: {
-    // width: 40,
-    // height: 40,
-  },
-  imge_container: {
-    backgroundColor: '#fafafa',
-    // backgroundColor: 'green',
-    width: 80,
-    height: 80,
-    // padding: 10,
-    // borderRadius: '0%',
-    alignItems: 'center',
-    borderRadius: 40,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginRight: 15,
-  },
-  youtube_Container: {
-    flexDirection: 'row',
-    // backgroundColor: 'red',
-    alignItems: 'center',
-    marginTop: 30,
-    marginLeft: 30,
-  },
-  youtubeText: {
-    fontSize: 18,
-    fontFamily: 'InterSemiBold',
-    color: '#000000',
-  },
-  youtubeText_below: {
-    fontSize: 14,
-    fontFamily: 'InterRegular',
-    color: '#666666',
-    // marginTop:8
-  },
-  below_conatent: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  line1: {
-    borderTopWidth: 1,
-    borderTopColor: '#DDDDDD',
-    marginVertical: 20,
-    // marginHorizontal: 30,
-  },
-  priceFeeConatiner: {
-    // backgroundColor:'brown',
-    marginTop: 43,
-    marginHorizontal: 30,
-  },
-  priceFeeText: {
-    color: '#666666',
-    fontSize: 16,
-    fontFamily: 'InterMedium',
-  },
-  TotalText: {
-    color: '#666666',
-    fontSize: 16,
-    fontFamily: 'InterSemiBold',
-  },
-  priceFeeValue: {
-    color: '#000000',
-    fontSize: 16,
-    fontFamily: 'InterMedium',
-  },
-  Totalvalue: {
-    color: '#000000',
-    fontSize: 16,
-    fontFamily: 'InterSemiBold',
-  },
-  priceText: {
-    marginBottom: 12,
-  },
-  card_container: {
-    // height: 100,
-    // width: 344,
-    backgroundColor: '#4388831a',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    borderRadius: 20,
-    // marginBottom:15,
-    marginHorizontal: 35,
-  },
-  circle_bank: {
-    marginTop: 15,
-    marginBottom: 15,
-    marginLeft: 20,
-    marginRight: 15,
-    width: 60,
-    height: 60,
-    backgroundColor: '#ffffff',
-    borderRadius: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  bankLink: {
-    fontSize: 16,
-    color: '#438883',
-    fontFamily: 'InterSemiBold',
-  },
-  paypalLink: {
-    fontSize: 16,
-    color: '#888888',
-    fontFamily: 'InterSemiBold',
-  },
-  othertext_contents: {
-    color: '#438883',
-    fontSize: 12,
-    fontFamily: 'InterMedium',
-    marginRight: 50,
-    marginTop: 4,
-  },
-  circletck: {
-    marginRight: 20,
-  },
-  card_container_paypal: {
-    // height: 100,
-    // width: 344,
-    backgroundColor: '#FAFAFA',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    borderRadius: 20,
-    // marginBottom:15,
-    marginHorizontal: 35,
-  },
-  selectMethod:{
-    marginTop:42,
-    marginBottom:20,
-    marginLeft:30
-  },
-  selectMethodText:{
-   color:'#000000',
-   fontSize:18,
-   fontFamily:'InterMedium'
-  },
-  bttn:{
-    while:358,
-    height:60,
-    backgroundColor:'#69AEA9',
-    borderRadius:40,
-    paddingHorizontal:20,
-    paddingVertical:10,
-    justifyContent:'center',
-  alignItems:'center',
-  marginHorizontal:28,
-  marginTop:30,
-  marginBottom:20
-  },
-  bttnText:{
-   color:'#ffffff',
-   fontSize:18,
-   fontFamily:'InterSemiBold',
-  
-  },
+      marginTop: 165,
+      borderTopLeftRadius: 30,
+      borderTopRightRadius: 30,
+    },
 
+    youtube_Container: {
+      flexDirection: 'row',
 
+      alignItems: 'center',
+      marginTop: width > 400 ? 30 : 20,
+      marginLeft: 30,
+    },
+    imge_container: {
+      backgroundColor: '#fafafa',
+      width: width > 400 ? 80 : 60,
+      height: width > 400 ? 80 : 60,
+      alignItems: 'center',
+      borderRadius: 40,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginRight: 15,
+    },
+    youtube_image: {
+      width: width > 400 ? 40 : 35,
+      height: width > 400 ? 40 : 35,
+    },
 
+    youtubeText: {
+      fontSize: width > 400 ? 18 : 14,
+      fontFamily: 'InterSemiBold',
+      color: '#000000',
+    },
+    billText: {
+      color: '#ffffff',
+      fontSize: width > 400 ? 18 : 14,
+      fontFamily: 'InterSemiBold',
+    },
+    youtubeText_below: {
+      fontSize: width > 400 ? 14 : 12,
+      fontFamily: 'InterRegular',
+      color: '#666666',
+    },
+    below_conatent: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    line1: {
+      borderTopWidth: 1,
+      borderTopColor: '#DDDDDD',
+      marginVertical: width > 400 ? 20 : 15,
+    },
+    priceFeeConatiner: {
+      marginTop: width > 400 ? 43 : 20,
+      marginHorizontal: 30,
+    },
+    priceFeeText: {
+      color: '#666666',
+      fontSize: width > 400 ? 16 : 14,
+      fontFamily: 'InterMedium',
+    },
+    TotalText: {
+      color: '#666666',
+      fontSize: width > 400 ? 16 : 14,
+      fontFamily: 'InterSemiBold',
+    },
+    priceFeeValue: {
+      color: '#000000',
+      fontSize: width > 400 ? 16 : 14,
+      fontFamily: 'InterMedium',
+    },
+    Totalvalue: {
+      color: '#000000',
+      fontSize: width > 400 ? 16 : 14,
+      fontFamily: 'InterSemiBold',
+    },
+    priceText: {
+      marginBottom: 12,
+    },
+    card_container: {
+      backgroundColor: '#4388831a',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      borderRadius: 20,
 
-  eelispse: {
-    position: 'absolute',
-   
-  },
-  images_conatiner: {
-    flex:1,
-    position:'absolute',
-    width:'100%',
-    flexDirection: 'row',
-    justifyContent: 'space-between', 
-    alignItems:'center',
-    marginTop:78,
-  },
+      marginHorizontal: 35,
+    },
+    circle_bank: {
+      marginTop: 15,
+      marginBottom: 15,
+      marginLeft: 20,
+      marginRight: 15,
+      width: width > 400 ? 60 : 45,
+      height: width > 400 ? 60 : 45,
+      backgroundColor: '#ffffff',
+      borderRadius: 30,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    bankLink: {
+      fontSize: width > 400 ? 16 : 12,
+      color: '#438883',
+      fontFamily: 'InterSemiBold',
+    },
+    paypalLink: {
+      fontSize: width > 400 ? 16 : 12,
+      color: '#888888',
+      fontFamily: 'InterSemiBold',
+    },
+    othertext_contents: {
+      color: '#438883',
+      fontSize: 12,
+      fontFamily: 'InterMedium',
+      marginRight: 50,
+      marginTop: 4,
+    },
+    circletck: {
+      marginRight: 20,
+    },
+    card_container_paypal: {
+      backgroundColor: '#FAFAFA',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      borderRadius: 20,
 
-  notification: {
-    width: 40,
-    height: 40,
-    borderRadius: 6.67,
-    padding: 8.33,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginRight:24,
-  },
-  lessthen:{
-    marginLeft:24,
-  },
-});
+      marginHorizontal: 35,
+      marginTop: 15,
+    },
+    selectMethod: {
+      marginTop: width > 400 ? 42 : 25,
+      marginBottom: 20,
+      marginLeft: 30,
+    },
+    selectMethodText: {
+      color: '#000000',
+      fontSize: width > 400 ? 18 : 14,
+      fontFamily: 'InterMedium',
+    },
+    bttn: {
+      width: width > 400 ? 358 : '85%',
+      height: width > 400 ? 60 : 45,
+      backgroundColor: '#69AEA9',
+      borderRadius: 40,
+      paddingHorizontal: 20,
+      paddingVertical: 10,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginHorizontal: 28,
+      marginTop: width > 400 ? 30 : 15,
+    },
+    bttnText: {
+      color: '#ffffff',
+      fontSize: width > 400 ? 18 : 14,
+      fontFamily: 'InterSemiBold',
+    },
+
+    eelispse: {
+      position: 'absolute',
+    },
+    images_conatiner: {
+      flex: 1,
+      position: 'absolute',
+      width: '100%',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      marginTop: 78,
+    },
+
+    notification: {
+      width: 40,
+      height: 40,
+      borderRadius: 6.67,
+      padding: 8.33,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginRight: 24,
+    },
+    lessthen: {
+      marginLeft: 24,
+    },
+  });
+}
--- src/screens/wallet/BillPaymentSuccessful.jsx
+++ src/screens/wallet/BillPaymentSuccessful.jsx
@@ -1,8 +1,15 @@
-import {StyleSheet, Text, View, SafeAreaView, Image,TouchableOpacity} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  useWindowDimensions,
+  Dimensions,
+} from 'react-native';
 import React from 'react';
-import WalletComponentScreen from '../../component/transcationComponent/WalletComponentScreen';
-// import { SafeAreaView } from 'react-native-safe-area-context'
-// import UpSvg from '../../assets/Images/upSvg.svg';
+
 import UpIcon from '../../assets/Images/upIcon.svg';
 import PaymentTick from '../../assets/Images/paymentTick.svg';
 
@@ -12,6 +19,7 @@ import Notification from '../../assets/Images/notification.svg';
 import LessThan from '../../assets/Images/lessThan.png';
 
 const BillPaymentSuccessful = ({navigation}) => {
+  const styles = useStyles();
   return (
     <SafeAreaView>
       <View>
@@ -22,6 +30,9 @@ const BillPaymentSuccessful = ({navigation}) => {
         <View style={styles.images_conatiner}>
           <View style={styles.lessthen}>
             <Image style={{width: 23, height: 23}} source={LessThan} />
+          </View>
+          <View>
+            <Text style={styles.billPaymet}>Bill Payment</Text>
           </View>
           <View style={styles.notification}>
             <Notification />
@@ -41,17 +52,10 @@ const BillPaymentSuccessful = ({navigation}) => {
           <Text style={styles.value}>Youtube Premium</Text>
         </View>
         {/* style={{marginTop:42}} */}
-        <View style={{marginTop: 32}}>
+        <View style={styles.transtionContainer_main}>
           <View style={styles.indivdial_contianer}>
             <View>
-              <Text
-                style={{
-                  fontSize: 18,
-                  fontFamily: 'InterMedium',
-                  color: '#000000',
-                }}>
-                Transaction details
-              </Text>
+              <Text style={styles.transtionText}>Transaction details</Text>
             </View>
             <View>
               <UpIcon />
@@ -130,9 +134,9 @@ const BillPaymentSuccessful = ({navigation}) => {
         </View>
 
         <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-        <View style={styles.bttn}>
-          <Text style={styles.bttnText}>Share Reciept</Text>
-        </View>
+          <View style={styles.bttn}>
+            <Text style={styles.bttnText}>Share Reciept</Text>
+          </View>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
@@ -141,132 +145,138 @@ const BillPaymentSuccessful = ({navigation}) => {
 
 export default BillPaymentSuccessful;
 
-const styles = StyleSheet.create({
-  container: {
-    position: 'absolute',
-    backgroundColor: 'white',
+function useStyles() {
+  const {width, height} = useWindowDimensions();
+  // console.log()
+  return StyleSheet.create({
+    container: {
+      position: 'absolute',
+      backgroundColor: 'white',
 
-    width: '100%',
+      width: '100%',
 
-    marginTop: 156,
-    borderTopLeftRadius: 30,
-    borderTopRightRadius: 30,
-  },
-  up_container: {
-    width: 80,
-    height: 80,
-    backgroundColor: '#fafafa',
-    borderRadius: 40,
-    marginTop: 20,
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  up: {},
-  income_container: {
-    marginTop: 10,
-  },
-  textt: {
-    color: '#438883',
-    fontFamily: 'InterSemiBold',
-    fontSize: 22,
-  },
-  value: {
-    color: '#666666',
-    fontFamily: 'InterMedium',
-    fontSize: 16,
-  },
-  indivdial_contianer: {
-    flexDirection: 'row',
-    marginHorizontal: 30,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  container_text: {
-    color: '#666666',
-    fontSize: 16,
-    fontFamily: 'InterMedium',
-  },
-  from_container: {
-    marginTop: 21,
-  },
-  other_container: {
-    marginTop: 12,
-  },
-  container_text_other: {
-    color: '#000000',
-    fontFamily: 'InterMedium',
-    fontSize: 16,
-  },
-  container_text_other_income: {
-    color: '#438883',
-    fontFamily: 'InterSemiBold',
-    fontSize: 16,
-  },
-  container_text_total: {
-    fontFamily: 'InterSemiBold',
-    fontSize: 16,
-    color: '#666666',
-  },
-  container_text_other_total: {
-    fontFamily: 'InterSemiBold',
-    fontSize: 16,
-    color: '#000000',
-    fontWeight: '600',
-  },
-  line1: {
-    borderTopWidth: 1,
-    borderTopColor: '#DDDDDD',
-    marginVertical: 15,
-    marginHorizontal: 30,
-  },
-  bttn:{
-    while:358,
-    height:60,
-    backgroundColor:'#69AEA9',
-    borderRadius:40,
-    paddingHorizontal:20,
-    paddingVertical:10,
-    justifyContent:'center',
-  alignItems:'center',
-  marginHorizontal:28,
-  marginTop:30,
-  marginBottom:20
-  },
-  bttnText:{
-   color:'#ffffff',
-   fontSize:18,
-   fontFamily:'InterSemiBold',
-  
-  },
-  download: {
-    color: '#438883',
-    fontFamily: 'InterSemiBold',
-    fontSize: 18,
-  },
-  eelispse: {
-    position: 'absolute',
-  },
-  images_conatiner: {
-    flex: 1,
-    position: 'absolute',
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginTop: 78,
-  },
+      marginTop: 156,
+      borderTopLeftRadius: 30,
+      borderTopRightRadius: 30,
+    },
+    up_container: {
+      width: width > 400 ? 80 : 60,
+      height: width > 400 ? 80 : 60,
+      backgroundColor: '#fafafa',
+      borderRadius: 40,
+      marginTop: width > 400 ? 20 : 15,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    income_container: {
+      marginTop: width > 400 ? 10 : 8,
+    },
+    textt: {
+      color: '#438883',
+      fontFamily: 'InterSemiBold',
+      fontSize: width > 400 ? 22 : 16,
+    },
+    value: {
+      color: '#666666',
+      fontFamily: 'InterMedium',
+      fontSize: width > 400 ? 16 : 12,
+    },
+    indivdial_contianer: {
+      flexDirection: 'row',
+      marginHorizontal: 30,
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+    container_text: {
+      color: '#666666',
+      fontSize: width > 400 ? 16 : 12,
+      fontFamily: 'InterMedium',
+    },
+    from_container: {
+      marginTop: width > 400 ? 21 : 15,
+    },
+    other_container: {
+      marginTop: 12,
+    },
+    container_text_other: {
+      color: '#000000',
+      fontFamily: 'InterMedium',
+      fontSize: width > 400 ? 16 : 12,
+    },
+    container_text_other_income: {
+      color: '#438883',
+      fontFamily: 'InterSemiBold',
+      fontSize: width > 400 ? 16 : 12,
+    },
+    container_text_total: {
+      fontFamily: 'InterSemiBold',
+      fontSize: width > 400 ? 16 : 12,
+      color: '#666666',
+    },
+    transtionText: {
+      fontSize: width > 400 ? 18 : 14,
+      fontFamily: 'InterMedium',
+      color: '#000000',
+    },
+    transtionContainer_main: {marginTop: width > 400 ? 32 : 24},
+    container_text_other_total: {
+      fontFamily: 'InterSemiBold',
+      fontSize: width > 400 ? 16 : 12,
+      color: '#000000',
+      fontWeight: '600',
+    },
+    line1: {
+      borderTopWidth: 1,
+      borderTopColor: '#DDDDDD',
+      marginVertical: width > 400 ? 15 : 10,
+      marginHorizontal: 30,
+    },
+    bttn: {
+      width: width > 400 ? 358 : '85%',
+      height: width > 400 ? 60 : 45,
+      backgroundColor: '#69AEA9',
+      borderRadius: 40,
+      paddingHorizontal: 20,
+      paddingVertical: 10,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginHorizontal: 28,
+      marginTop: width > 400 ? 30 : 15,
+    },
+    bttnText: {
+      color: '#ffffff',
+      fontSize: width > 400 ? 18 : 14,
+      fontFamily: 'InterSemiBold',
+    },
 
-  notification: {
-    width: 40,
-    height: 40,
-    borderRadius: 6.67,
-    padding: 8.33,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginRight: 24,
-  },
-  lessthen: {
-    marginLeft: 24,
-  },
-});
+    eelispse: {
+      position: 'absolute',
+    },
+    images_conatiner: {
+      flex: 1,
+      position: 'absolute',
+      width: '100%',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      marginTop: 78,
+    },
+    billPaymet: {
+      color: '#ffffff',
+      fontSize: width > 400 ? 18 : 14,
+      fontFamily: 'InterSemiBold',
+    },
+    notification: {
+      width: 40,
+      height: 40,
+      borderRadius: 6.67,
+      padding: 8.33,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginRight: 24,
+    },
+    lessthen: {
+      marginLeft: 24,
+    },
+  });
+}
